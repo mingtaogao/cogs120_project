fbd563a646ec361f76303e39770101304f77d9b1
Mingtao Gao <mig049@ucsd.edu>
2018-11-08 00:58:22 -0800

    modified

--- js/tutorials.js
+++ js/tutorials.js
@@ -1,4 +1,12 @@
-$('#video').on('click', function() {
-  $(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/8Jqof7z3QYM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+$('.legExt').on('click', function() {
+  $(this).html('<iframe width="110" height="90" src="https://www.youtube.com/embed/8Jqof7z3QYM" frameborder="0" allowfullscreen="true"></iframe>')
+  .addClass('active');
+});
+$('.calfExt').on('click', function() {
+  $(this).html('<iframe width="110" height="90" src="https://www.youtube.com/embed/8Jqof7z3QYM" frameborder="0" allowfullscreen="true"></iframe>')
+  .addClass('active');
+});
+$('.tricepPress').on('click', function() {
+  $(this).html('<iframe width="110" height="90" src="https://www.youtube.com/embed/4W31U8tTVAg" frameborder="0" allowfullscreen="true"></iframe>')
   .addClass('active');
 });
